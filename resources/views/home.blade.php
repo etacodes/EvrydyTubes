06f83f473b388e558b6a2c63fde3260224a3a6bb
etacodes <erickotimur2002@gmail.com>
2022-12-06 22:45:07 +0700

    commit navbar 1

---
+++ resources/views/home.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>EVRYDY | Home</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    {{-- bootstrap icons --}}
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+  </head>
+  <body>
+        <nav class="navbar navbar-expand-lg bg-light">
+                <div class="container">
+                  <a class="navbar-brand" href="#">
+                    <img src="img/B.jpg" alt="" width="30" height="30" class="d-inline-block align-text-top">
+                    EverydayShoes
+                  </a>
+                  <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                  </button>
+                  <div class="collapse navbar-collapse" id="navbarNav">
+                    <ul class="navbar-nav">
+                      <li class="nav-item">
+                        <a class="nav-link active" href="/"><i class="bi bi-house-door"></i> Home</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/review"><i class="bi bi-view-list"></i> Review</a>
+                      </li>
+                      <li class="nav-item">
+                        <a class="nav-link" href="/brand"><i class="bi bi-border-all"></i> Brand</a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </nav>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  </body>
+</html>
